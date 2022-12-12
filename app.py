5221d7bf9eaaf736e7ba390ebe88de10a937e92f
elior <elior1001@gmail.com>
2022-12-12 14:59:59 +0200

    small comment about post method

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 app = Flask(__name__)
+
+
 #curl -X POST -d 'direction=in&truck=t1&containers=['c-1', 'c2', 'c3']&weight=200&unit=kg&force=something&produce=apples' http://localhost:5000/weight
 #curl -X POST -d 'container=123&blabla=abc' http://localhost:5000/weight
+
+#This is the Post Mehthod
 @app.route("/weight", methods=["POST"])
 def weightpost():
     if request.method == "POST":
