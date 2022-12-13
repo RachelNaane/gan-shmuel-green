e18cb48142977a4e6d715e3238366775b0194243
elior <elior1001@gmail.com>
2022-12-13 20:33:01 +0200

    Get /session by eyal!

--- weight/app.py
+++ weight/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
-from datetime import datetime
+from datetime import datetime, date
+import datetime
 from db import dbconnection
 import json, re
 
@@ -58,38 +59,73 @@ def weightpost():
         # print(truckidlist[-1])
         return "allrows"
         
+
 #Get Weight return json of the last time according to a time zone
-#need to improve so it works with time
-#need to improve the Filt so it works with all options
 @app.route("/weight", methods=["GET"])
 def weightget():
-    date = request.args.get("from")
-    hour = request.args.get("to")
-    filt = request.args.get("filter")
+    #ARGS PARSE from url
+    #Time Looks like yyyymmddhhmmss
     
-    #geting the values from the database
-    id1 = dbconnection.run_sql_command("select id from transactions")
-    print(filt)
+    date_start = request.args.get("from")
+    date_end = request.args.get("to")
+    #filter
+    filt = request.args.get("filter")
+
+    #Getting the date from the args
+    if date_start is not None:
+        timestamp = str(date_start)
+        timestamp = timestamp.zfill(14)
+        # Create a datetime object from the timestamp
+        date_start = datetime.datetime.strptime(timestamp, '%Y%m%d%H%M%S')
+        date_start = date_start.strftime('%Y-%m-%d %H:%M:%S')
+
+    if date_end is not None:
+        timestamp_end = str(date_end)
+        timestamp_end = timestamp_end.zfill(14)
+        date_end = datetime.datetime.strptime(timestamp_end, '%Y%m%d%H%M%S')
+        date_end = date_end.strftime('%Y-%m-%d %H:%M:%S')
+
+    if date_start is None:
+        #give value of 0000 and the date of today
+        tmp1 = str(date.today())
+        date_start = tmp1+" 00:00:00"
+
+    if date_end is None:
+        #value of now:
+        current_time = datetime.datetime.now()
+        date_end = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    #Adjust the query according to the filter
     if filt is None:
-        query = dbconnection.run_sql_command("select * from transactions")
-    elif "in" in filt:
-        query = dbconnection.run_sql_command("select * from transactions where direction = in")
-    elif filt == "out":
-        query = dbconnection.run_sql_command("select * from transactions where direction = out")
+        query = dbconnection.run_sql_command(fr"select * from transactions where datetime >= '{date_start}' AND datetime <= '{date_end}';")
+    else:
+        times = fr"AND datetime >= '{date_start}' AND datetime <= '{date_end}'"
+        query = []
+        filter_all = filt.split(',')
+        if "in" in filter_all:
+            query += dbconnection.run_sql_command(f"select * from transactions where direction = 'in' {times}")
+        if "out" in filter_all:
+            query += dbconnection.run_sql_command(f"select * from transactions where direction = 'out' {times}")
+        if "none" in filter_all:
+            query += dbconnection.run_sql_command(f"select * from transactions where direction = 'none' {times}")
 
     item_list = []
     single_item = {}
+   #PUTTING RESOLT INTO A LIST
+    query = [i for i in query]
     for item in query:
         # tracklist.append(item[3])
-        single_item.update({
+        # print(item)
+        single_item = {
             "id":item[0],
-            "direction":item[1],
-            "bruto":item[2],
-            "neto":item[3],
-            "produce":item[4],
-            "containers":item[5]
-        })
+            "direction":item[3],
+            "bruto":item[4],
+            "neto":item[5],
+            "produce":item[6],
+            "containers":item[7]
+        }
         item_list.append(single_item)
+   
     return item_list
 
 
@@ -97,7 +133,6 @@ def weightget():
 @app.route("/health")
 def health():
     return "OK\n"
-
 
 
 #starting of implemanation
@@ -112,70 +147,29 @@ def get_item(id):
     
     #Args are Ok for easy development
 
-
     #Query the Db for this is ID
     #track_Tara = dbconnection.run_sql_command(f"select truckTara from transactions where id={id_input}")
     
-
     return f"{id_input}:{from_arg}{to_arg}"
 
 
-
-
-###############################################################################################
-# GET /session/<id>
-# - id is for a weighing session. 404 will be returned if non-existent
-# Returns a json:
-#  { "id": <str>,
-#    "truck": <truck-id> or "na",
-#    "bruto": <int>,
-#    ONLY for OUT:
-#    "truckTara": <int>,
-#    "neto": <int> or "na" // na if some of containers unknown
-#  }
-# GET /session/<id>
-
-#Missons:
-# Return without tuples (only the string in the dictonary )
 @app.route("/session/<id>", methods=["GET"])
 def get_session(id):
-
-    return_dict = {}
-    #Check for id in the db IF not return 404
-    # dbconnection.run_sql_command(f"select id from transactions where id={id}")
-    res = dbconnection.run_sql_command(f"select id from transactions where id={id}")
-    if res == []:
+    alldata = dbconnection.run_sql_command(f"select * from transactions where sessionid={id}")
+    listdata = [i for i in alldata]
+    print (listdata)
+    # dict_out = {}
+    if len(listdata) == 0:
         return abort(404)
 
-
-    ##values for the json
-    truck_id = dbconnection.run_sql_command(f"select truck from transactions where id={id}")
-    bruto = dbconnection.run_sql_command(f"select bruto from transactions where id={id}")
-
-    #Check for in and out
-    state = dbconnection.run_sql_command(f"select direction from transactions where id={id}")
-    print(f"{truck_id} {bruto} {state}")
-    return_dict.update({
-        "id":f"{id}",
-        "truck":f"{truck_id}",
-        "bruto":f"{bruto}"
-    })
-
-    print(state)
-    #format state
-    if "out" in state:
-        #should add more values to the json tracktar,neto
-        track_tara = dbconnection.run_sql_command(f"select truckTara from transactions where id={id}")
-        neto = dbconnection.run_sql_command(f"select neto from transactions where id={id}")
-        return_dict.update({
-            "truckTara":track_tara,
-            "neto":neto
-        })
-
-    print(f"{truck_id}{bruto}")
-
-
-    return json.dumps(return_dict)
+    if listdata[-1][3] == 'out':
+          dict_out = {"id":id , "truck":listdata[-1][4] , "bruto":listdata[0][6],"tara":listdata[-1][7],"neto":listdata[-1][8]}
+    elif listdata[0][3] == 'in':
+           dict_out = {"id":id , "truck":listdata[0][4] , "bruto":listdata[0][6]}
+    else: 
+          dict_out = {"id":id ,"truck":"N/A" ,"container":listdata[0][5] , "neto":listdata[0][8]}
+         
+    return dict_out
 
 # GET /unknown
 # Returns a list of all recorded containers that have unknown weight:
@@ -191,8 +185,6 @@ def return_unkown_containers():
 
 
     return list_of_unkown
-
-
 
 
 
--- weight/db/sample.py
+++ weight/db/sample.py
@@ -8,8 +8,6 @@ dbconnection.run_inset_query(fr"INSERT INTO transactions (id, sessionid, datetim
 dbconnection.run_inset_query(fr"INSERT INTO transactions (id, sessionid, datetime, direction, truck, containers, bruto, truckTara, neto, produce) VALUES ('1225025', '12323', '2022-11-11', 'out', '123', '5423', '432', '542', '433', '431')")
 
 
-
-
 dbconnection.run_inset_query(fr"INSERT INTO containers_registered (container_id, weight, unit) VALUES ('C-35334', '296', 'kg');")
 dbconnection.run_inset_query(fr"INSERT INTO containers_registered (container_id, unit) VALUES ('C-23134', 'kg');")
 dbconnection.run_inset_query(fr"INSERT INTO containers_registered (container_id, unit) VALUES ('C-28884', 'kg');")
