8c191c0ef65e9f6cee7f79f8e18735919fe1c8da
RachelNaane <ratash3@gmail.com>
2022-12-14 12:05:29 +0200

    changes in phase2

--- devops/ci-continer/phase2.sh
+++ devops/ci-continer/phase2.sh
@@ -6,16 +6,15 @@ root_devops=/home/ubuntu/gan-shmuel-green/devops/ci-continer
 
 cd $root_billing 
 docker compose down
-export APP_PORT=8083
-export DB_PORT=8084
 
+echo -e "APP_PORT=8083\nDB_PORT=8084" > .env
 docker compose up -d
 wait
 
 cd $root_weight
 docker compose down
-export APP_PORT=8081
-export DB_PORT=8082
+
+echo -e "APP_PORT=8081\nDB_PORT=8082" > .env
 docker compose up -d
 wait
 
