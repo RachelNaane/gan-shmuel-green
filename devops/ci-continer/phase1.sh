fecb38877758c2c8465b1d3e0beeca210669bb70
RachelNaane <ratash3@gmail.com>
2022-12-14 11:17:48 +0200

    changes in phase1 and phase2

--- devops/ci-continer/phase1.sh
+++ devops/ci-continer/phase1.sh
@@ -1,34 +1,61 @@
+#/bin/bash
 #####  1) will do git clone inside test folder and will uplaoad a testing env'.
 #####  2) will save the result in the file score.txt (OK=passes, ERROR = faild).
 #####  3) will close all the test env' and delete the folder test.
 
-mkdir test && cd test
-git clone https://github.com/RachelNaane/gan-shmuel-green.git
-wait
-cd gan-shmuel-green 
-cd billing && docker compose build -no-cache && docker compose up # add port and network envs 
-wait
-cd -
-cd weight && docker compose build -no-cache && docker compose up # add port and network envs
-wait
-cd -
-bash billing/test/test.sh
-exitcode_billing=$?
-bash weight/test/test.sh
-exitcode_weight=$?
+root_billing=/home/ubuntu/gan-shmuel-green/billing
+root_weight=/home/ubuntu/gan-shmuel-green/weight
+root_devops=/home/ubuntu/gan-shmuel-green/devops/ci-continer
 
-cd ~/
-rm -f -r test
-# to add the if from phase2 to here
-if [ $(($exitcode_billing)) -eq 0 ] && [ $(($exitcode_billing)) -eq 0 ]
-then
-#run script phase2
-#echo "message">> billing/score.txt
-#echo "message">> weight/score.txt
-else
-#echo "message">> score.txt
+mkdir test && cd test
+git clone https://github.com/RachelNaane/gan-shmuel-green.git 
+wait
+
+#up env-testing
+cd $root_weight
+export APP_PORT=8086
+export DB_PORT=8087
+docker compose build -no-cache && docker compose up -d # add network envs
+wait
+
+cd $root_billing
+export APP_PORT=8088
+export DB_PORT=8089
+docker compose build -no-cache && docker compose up -d # add network envs 
+wait
+
+#run test
+bash $root_weight/tests/test.sh > score.txt
+exitcode_weight=$?
+bash $root_billing/tests/test.sh > score.txt
+exitcode_billing=$?
+
+#down env-testing
+cd $root_weight
+docker compose down -v
+wait
+
+cd $root_billing
+docker compose down -v
+wait
+
+#deleate test dir
+cd $root_devops
+rm -fr test
+
+# add conclusion message to report
+message="CI RESULT: "
+billing_result="billing test-failure"
+weight_result="weight test-failure"
+conclusion="cant run this version in production. please fix bugs"
+
+if [ $exitcode_billing -eq 0 ]; then billing_result="billing test-success"; fi
+if [ $exitcode_billing -eq 0 ]; then weight_result="weight test-success"; fi
+if [ $exitcode_billing -eq 0 ] && [ $exitcode_weight -eq 0 ]; then
+    #run new version on production!
+    phase2.sh
+    conclusion="new version up in production. good work everyone!"
 fi
 
-
-
-
+echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $root_billing/score.txt
+echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $root_weight/score.txt
--- devops/ci-continer/phase2.sh
+++ devops/ci-continer/phase2.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-value=$(cat exitcode.txt)
-echo "$value"
+root_billing=/home/ubuntu/gan-shmuel-green/billing
+root_weight=/home/ubuntu/gan-shmuel-green/weight
+root_devops=/home/ubuntu/gan-shmuel-green/devops/ci-continer
 
-if [ $(($value)) -eq 0 ]
-then
-    # remove last working production
-    cd ~/gan-shmuel-green
-    cd billing && docker compose down -v
-    wait
-    cd -
-    cd weight && docker compose down -v
-    wait
-    cd -
-    # pulling and uploading the new production
-    git pull origin main
-    wait
-    cd billing && docker compose up # add port and netwok envs
-    wait
-    cd -
-    cd weight && docker compose up # add port and netwok envs
-    wait
-    cd -
-fi
+cd $root_billing 
+docker compose down
+export APP_PORT=8083
+export DB_PORT=8084
+
+docker compose up -d
+wait
+
+cd $root_weight
+docker compose down
+export APP_PORT=8081
+export DB_PORT=8082
+docker compose up -d
+wait
+
 # check to run docker compose through socket
