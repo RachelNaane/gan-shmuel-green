62e28ca66e9955a40777f9c35591fa74e1db6b15
mayoloving <yota.benz@outlook.com>
2022-12-15 10:49:43 +0200

    echoing cd failure into score.txt

--- devops/ci-continer/phase1.sh
+++ devops/ci-continer/phase1.sh
@@ -3,46 +3,48 @@
 #####  2) will save the result in the file score.txt (OK=passes, ERROR = faild).
 #####  3) will close all the test env' and delete the folder test.
 
+# full paths
 root_billing=/app/test/gan-shmuel-green/billing
 root_weight=/app/test/gan-shmuel-green/weight
-#root_devops=/app/test/gan-shmuel-green/devops/ci-continer
-#/app/test/gan-shmuel-green/billing/tests/test.sh
+full_score_path_weight=/app/test/gan-shmuel-green/weight/tests/score.txt
+full_score_path_billing=/app/test/gan-shmuel-green/billing/tests/score.txt
+
 mkdir test 
-cd test || { echo "cd failed"; exit 1; }
+cd test || { echo "'cd test' failed "; exit 1; }
 git clone https://github.com/RachelNaane/gan-shmuel-green.git 
 wait
 
-#up env-testing
-cd $root_weight || { echo "cd failed"; exit 1; }
+# up env-testing
+cd $root_weight || { echo "'cd $(root_weight)' the current path is $(pwd)" |tee $full_score_path_weight $full_score_path_billing ; exit 1; }
 
 echo -e "APP_PORT=8086\nDB_PORT=8087\nVOLUME=weight-test-volume" > .env
 docker compose build -no-cache && docker compose up -d # -f for specifing compose file?
 wait
 
 
-cd $root_billing || { echo "cd failed"; exit 1; }
+cd $root_billing || { echo "'cd $(root_billing)' the current path is $(pwd)" |tee $full_score_path_weight $full_score_path_billing ; exit 1; }
 
 echo -e "APP_PORT=8088\nDB_PORT=8089\nVOLUME=billing-test-volume" > .env
 docker compose build -no-cache && docker compose up -d # -f for specifing compose file?
 wait
 
-#run test
-bash $root_weight/tests/test.sh #> $root_weight/tests/score.txt
+# run test
+bash $root_weight/tests/test.sh
 exitcode_weight=$?
-bash $root_billing/tests/test.sh #> $root_billing/tests/score.txt
+bash $root_billing/tests/test.sh
 exitcode_billing=$?
 
-#down env-testing
-cd $root_weight || { echo "cd failed"; exit 1; }
+# down env-testing
+cd $root_weight || { echo "'cd $(root_weight)' the current path is $(pwd)" |tee $full_score_path_weight $full_score_path_billing ; exit 1; }
 docker compose down -v
 wait
 
-cd $root_billing || { echo "cd failed"; exit 1; }
+cd $root_billing || { echo "'cd $(root_billing)' the current path is $(pwd)" |tee $full_score_path_weight $full_score_path_billing ; exit 1; }
 docker compose down -v
 wait
 
-#delete test dir
-cd /app || { echo "cd failed"; exit 1; }
+# delete test dir
+cd /app || { echo "'cd /app' failed"; exit 1; }
 rm -fr test
 
 # add conclusion message to report
@@ -51,13 +53,14 @@ billing_result="billing test-failure"
 weight_result="weight test-failure"
 conclusion="cant run this version in production. please fix bugs"
 
+# checking exitcode 
 if [ $exitcode_billing -eq 0 ]; then billing_result="billing test-success"; fi
 if [ $exitcode_weight -eq 0 ]; then weight_result="weight test-success"; fi
 if [ $exitcode_billing -eq 0 ] && [ $exitcode_weight -eq 0 ]; then
-    #run new version on production!
+    # run new version on production!
     bash phase2.sh
     conclusion="new version up in production. good work everyone!"
 fi
 
-echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $root_billing/tests/score.txt
-echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $root_weight/tests/score.txt
+echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $full_score_path_weight
+echo -e "\n\n$message \n$billing_result \n$weight_result \n$conclusion" >> $full_score_path_billing
