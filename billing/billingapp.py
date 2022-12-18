069445f785ec2c52c61655a65d0bd3f88dd890c3
R0EI <roei.keisar@gmail.com>
2022-12-18 09:12:12 +0200

    port has been fixed

--- billing/billingapp.py
+++ billing/billingapp.py
@@ -56,7 +56,7 @@ def get_truck_data(id):
     t1 = request.args.get('from', default= default_start)
     t2 = request.args.get('to', default= datetime)
 
-    response = requests.get(f"http://{curr_host}:8083/item/{id}?t1={t1}")
+    response = requests.get(f"http://{curr_host}:8081/item/{id}?t1={t1}")
     if response.status_code == 200:
         data = response.json()
         return data  
@@ -175,7 +175,7 @@ def bill(id):
         current_providers_trucks.append(truck[0])
     
     curr_host = "3.9.66.97"
-    weight_json_array = requests.get(f"http://{curr_host}:8083/weight?t1={start}&t2={to}")
+    weight_json_array = requests.get(f"http://{curr_host}:8081/weight?t1={start}&t2={to}")
     weight_json_array = weight_json_array.json()
 
     rev = {"id":id,"name": provider_name,"from": convert_int_to_correct_date_format(start),
@@ -198,7 +198,7 @@ def bill(id):
                 current_rate = current_rate[0]
             current_sessions_num = 0
             for container in weight["containers"]:
-                temp = requests.get(f"http://{curr_host}:8083/item/{container}?t1={start}")
+                temp = requests.get(f"http://{curr_host}:8081/item/{container}?t1={start}")
                 temp_container = temp.json()
                 current_sessions_num += len(temp_container["sessions"])
 
